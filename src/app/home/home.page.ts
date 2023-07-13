eff8a8407442ff99e7caf92b7a7f2dc77c97b218
Jose Ricardo Peixoto <peixoto.service@hotmail.com>
2023-07-12 21:57:42 -0300

    finalizando
    finalizado app simples em ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public resultado: String = "Resultado"
+  public precoAlcool: string = ""
+  public precoGasolina: string =""
+  calcular(){
+    if (this.precoAlcool && this.precoGasolina){
+      let pAlcool = parseFloat(this.precoAlcool)
+      let pGasolina = parseFloat(this.precoGasolina)
+      let res = pAlcool / pGasolina
+      if (res >= 0.7){
+        this.resultado = "Melhor usar Gasolina!"
+      }
+      else {
+        this.resultado = "Melhor Usar Álcool"
+      }
+    }else{
+      this.resultado = "Preencha corretamente os campos!"
+    }
+  }
 
   constructor() {}
 
